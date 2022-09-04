5f56c9add719903caebd26f9624610057367792c
Hyrondelle <n.hyr@free.fr>
2022-09-04 10:11:29 +0200

    color change

--- js/index.js
+++ js/index.js
@@ -4,3 +4,15 @@ const scroll = new LocomotiveScroll({
     tablet: {smooth : true},
     smartphone: {smooth : true},
 });
+scroll.on("scroll",()=>{
+    if(document.querySelector('#colortransition.is-inview')){
+        document.body.style.backgroundColor = "#000101";
+        document.body.style.color = "#fefeff";
+        
+    }
+    else{
+        document.body.style.backgroundColor = "#fefeff";
+        document.body.style.color = "#000101";
+        
+    }
+})
